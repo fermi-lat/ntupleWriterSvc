9c9e6784f81a3cd14639f8a967cfcc63e2a56e2c
heather <>
2001-05-14 23:09:00 +0000

    replaces ntupleWriter - hopefully the import will work correctly this time.

---
+++ ntupleWriterSvc/INtupleWriterSvc.h
@@ -0,0 +1,32 @@
+#ifndef _H_INTupleWriterSvc_
+#define _H_INTupleWriterSvc_
+
+// includes
+#include "GaudiKernel/IInterface.h"
+#include "GaudiKernel/NTuple.h"
+
+// Declaration of the interface ID ( interface id, major version, minor version) 
+static const InterfaceID IID_INTupleWriterSvc(1400, 1 , 0); 
+
+/*! Proper Gaudi abstract interface class for the ntupleWriterSvc 
+*/
+class INTupleWriterSvc : virtual public IInterface
+{  
+
+public:
+
+    virtual StatusCode initialize ()=0;
+    
+    virtual StatusCode finalize ()=0;
+
+    virtual SmartDataPtr<NTuple::Tuple> getNTuple(const char *tupleName)=0;
+
+    virtual StatusCode addItem(const char *tupleName, const char *item, const float val, bool first)=0;
+
+    /// Retrieve interface ID
+    static const InterfaceID& interfaceID() { return IID_INTupleWriterSvc; }
+
+};
+
+
+#endif // _H_INTupleWriterSvc
