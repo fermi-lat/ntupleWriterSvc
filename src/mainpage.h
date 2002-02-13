7e621a9ba7e5fb9c75b9f9d7e8e8c532d3afc567
heather <>
2002-02-13 19:06:49 +0000

    remove itoa, replace with ostringstream and move mainpage to src directory

---
+++ src/mainpage.h
@@ -0,0 +1,18 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/ntupleWriterSvc/ntupleWriterSvc/mainpage.h,v 1.1 2001/05/16 14:42:41 burnett Exp $
+// Mainpage for doxygen
+
+/** @mainpage package ntupleWriterSvc
+ * @author Heather Kelly
+ *
+ * @section intro Introduction
+ * This package is an interface to the Gaudi NTupleSvc.  Our GLAST
+ * algorithms use the ntupleWriterSvc to submit entries into the
+ * standard GLAST ntuple.
+ * For more information see the <a href=../ntupleWriterSvc.htm>manual</a>.
+ * <hr>
+ * @section notes release notes
+ * release.notes
+ * @section requirements requirements
+ * @verbinclude requirements
+*/
+
