8e8423cab240cf50cf00c6b89a2784598a8ab8a2
burnett <>
2004-06-22 18:46:45 +0000

    Remove ntupleWriterSvc, since we only use RootTupleSvc

--- src/Dll/ntupleWriterSvc_load.cxx
+++ src/Dll/ntupleWriterSvc_load.cxx
@@ -5,13 +5,12 @@
  * This routine is needed for forcing the linker to load all the 
  * components of the library. 
  * 
- * $Header: /nfs/slac/g/glast/ground/cvs/ntupleWriterSvc/src/Dll/ntupleWriterSvc_load.cxx,v 1.4 2002/04/08 21:13:20 heather Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/ntupleWriterSvc/src/Dll/ntupleWriterSvc_load.cxx,v 1.5 2003/07/23 16:38:01 burnett Exp $
  */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(ntupleWriterSvc) {
-    DECLARE_SERVICE(ntupleWriterSvc);
     DECLARE_SERVICE(RootTupleSvc);
 
     DECLARE_ALGORITHM(WriteTupleAlg);
