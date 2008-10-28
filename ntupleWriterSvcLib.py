0701b5a2e164005bf284db63e22b0d45b98d591a
ecephas <>
2008-10-28 16:59:34 +0000

    Changed 	env.Tool('addLibrary', library = 'ntupleWriterSvc') to         env.Tool('addLibrary', library = ['ntupleWriterSvc'])
    Added the following:
         env.Tool('facilitiesLib')
         env.Tool('addLibrary', library = env['gaudiLibs'])
         env.Tool('addLibrary', library = env['rootLibs'])

--- ntupleWriterSvcLib.py
+++ ntupleWriterSvcLib.py
@@ -1,8 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ntupleWriterSvc/ntupleWriterSvcLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'ntupleWriterSvc')
-
-
+        env.Tool('addLibrary', library = ['ntupleWriterSvc'])
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
