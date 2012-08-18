65c822ee3d47c8692268d00f18064bbf374e069e
jrb <>
2012-08-18 00:32:15 +0000

    support no-install headers on Windows

--- ntupleWriterSvcLib.py
+++ ntupleWriterSvcLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ntupleWriterSvc/ntupleWriterSvcLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ntupleWriterSvc/ntupleWriterSvcLib.py,v 1.2 2008/10/28 16:59:34 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['ntupleWriterSvc'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'ntupleWriterSvc') 
     env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('addLibrary', library = env['rootLibs'])
